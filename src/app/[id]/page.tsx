53d9788636ba72ae3b803271039f4d576f50e45a
julianParrax <julianfranco.parra@gmail.com>
2023-09-27 11:52:17 -0300

    Added: 01. Added view to get results from each id

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,21 @@
+import api from "@/api"
+import Link from "next/link"
 
-export default function IdPage({params: {id}}:{params:{id:string}}) {
+export default async function IdPage({params: {id}}:{params:{id:string}}) {
+  const caseStudy = await api.caseStudy.fetch(Number(id))
   return (
-    <div>{id}</div>
-  )
+    <article className="grid gap-8">
+      <Link href="/" className="opacity-80">
+      ← Back to list
+      </Link>
+      <hgroup className=" grid gap-2">
+          <img src={caseStudy.hero_image} alt={caseStudy.title} className="w-full object-cover" height={256} />
+          <div>
+            <h2 className="text-xl font-bold">{caseStudy.title}</h2>
+            <p className="opacity-80">{caseStudy.teaser}</p>
+          </div>
+        </hgroup>
+    </article>
+    )
 }
 
